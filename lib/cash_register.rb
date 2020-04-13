70a6063e90022fce1b4b773b91d67ddbcd079119
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-13 10:22:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
 
   def add_item(item, price, quantity=1)
-     @price = price
+     self.price = price
      @quantity = quantity
     @total += @price * @quantity
      quantity.times {@items << item}
